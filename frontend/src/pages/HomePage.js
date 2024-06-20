f8e34869d22bbbf17635cc4e34473252b09b5d29
Rexon <rexonpambujya2001@gmail.com>
2024-06-20 23:49:29 +0530

    pushing project

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import PageContent from '../components/PageContent';
+function HomePage() {
+    return (
+        <PageContent title="Welcome!">
+            <p>Browse All Amazing Events!</p>
+        </PageContent>
+    );
+}
+
+export default HomePage;
